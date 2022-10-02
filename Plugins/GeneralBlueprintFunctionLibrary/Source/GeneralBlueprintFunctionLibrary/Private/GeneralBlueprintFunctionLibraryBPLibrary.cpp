cd6634b7efe11e86a965790fb5985776a9594273
Matthew George <mustangs155222@gmail.com>
2022-10-02 00:01:35 -0400

    #172 Added general BP function library plugin from original project.

---
+++ Plugins/GeneralBlueprintFunctionLibrary/Source/GeneralBlueprintFunctionLibrary/Private/GeneralBlueprintFunctionLibraryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GeneralBlueprintFunctionLibraryBPLibrary.h"
+#include "GeneralBlueprintFunctionLibrary.h"
+
+UGeneralBlueprintFunctionLibraryBPLibrary::UGeneralBlueprintFunctionLibraryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGeneralBlueprintFunctionLibraryBPLibrary::GeneralBlueprintFunctionLibrarySampleFunction(float Param)
+{
+	return -1;
+}
+
