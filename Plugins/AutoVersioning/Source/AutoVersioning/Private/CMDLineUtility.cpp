acdda97ef3d10cbdd8dad597d73025578e727bb1
Matthew George <mustangs155222@gmail.com>
2024-03-08 00:26:43 -0500

    Trying to get the auto versioning tool to work without showing a window.

--- Plugins/AutoVersioning/Source/AutoVersioning/Private/CMDLineUtility.cpp
+++ Plugins/AutoVersioning/Source/AutoVersioning/Private/CMDLineUtility.cpp
@@ -2,7 +2,7 @@
 
 string CMDLineUtility::ExecCMD(string cmd)
 {
-    char buffer[128];
+    /*char buffer[128];
     string result = "";
 
     FILE* pipe = _popen(cmd.c_str(), "r");
@@ -14,5 +14,30 @@ string CMDLineUtility::ExecCMD(string cmd)
     }
 
     _pclose(pipe);
-    return result;
+    return result;*/
+
+    HANDLE stdInHandles[2];
+    HANDLE stdOutHandles[2];
+    HANDLE stdErrHandles[2];
+    CreatePipe(&stdInHandles[0], &stdInHandles[1], NULL, 4096);
+    CreatePipe(&stdOutHandles[0], &stdOutHandles[1], NULL, 4096);
+    CreatePipe(&stdErrHandles[0], &stdErrHandles[1], NULL, 4096);
+
+    STARTUPINFOA startupInfo = { 0 };
+    PROCESS_INFORMATION procInfo = { 0 };
+    startupInfo.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+    startupInfo.wShowWindow = SW_HIDE;
+    startupInfo.hStdInput = stdInHandles[0];
+    startupInfo.hStdOutput = stdOutHandles[1];
+    startupInfo.hStdError = stdErrHandles[1];
+    bool result = ::CreateProcessA("C:\\windows\\system32\\cmd.exe", const_cast<char*>(cmd.c_str()), NULL, NULL, false, 0, NULL, NULL, &startupInfo, &procInfo);
+    if (result == false)
+    {
+        DWORD error = ::GetLastError();
+        return "Error: " + to_string(error);
+    }
+
+    LPDWORD returnVal = new DWORD[1];
+    ::GetExitCodeProcess(procInfo.hProcess, returnVal);
+    return to_string(returnVal[0]);
 }
--- Plugins/AutoVersioning/Source/AutoVersioning/Public/CMDLineUtility.h
+++ Plugins/AutoVersioning/Source/AutoVersioning/Public/CMDLineUtility.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
