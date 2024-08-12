36972577dda6142ad9a3550b23e21d5f6b6d6cc4
Matthew George <mustangs155222@gmail.com>
2024-08-11 22:38:53 -0400

    Removed auto versioning plugin and updated gant chart and versioning tool settings.

--- Plugins/AutoVersioning/Source/AutoVersioning/Private/AutoVersioningBPFL.cpp
+++
@@ -1,9 +0,0 @@
-#include "AutoVersioningBPFL.h"
-#include "AutoVersioning.h"
-
-FString UAutoVersioningBPFL::UpdateAndApplyVersion()
-{
-	FAutoVersioningModule* autoVersioningModule = FModuleManager::GetModulePtr<FAutoVersioningModule>(FName("AutoVersioning"));
-	if (autoVersioningModule == NULL) return "ERROR: Couldn't find a 'FAutoVersioningModule' by name 'AutoVersioning'!";
-	return autoVersioningModule->UpdateAndApplyVersion().c_str();
-}
